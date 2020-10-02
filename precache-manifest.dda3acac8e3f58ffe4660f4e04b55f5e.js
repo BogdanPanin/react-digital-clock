63ec8e1d41bb1a743e9269cd78af6ef30d401f48
Your Name <bogdan.panin@gmail.com>
2020-10-02 19:59:28 +0300

    Updates

---
+++ precache-manifest.dda3acac8e3f58ffe4660f4e04b55f5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a049207027de7d0bd7782eb65972f2d8",
+    "url": "/react-digital-clock/index.html"
+  },
+  {
+    "revision": "bfbcabd31b36edb32776",
+    "url": "/react-digital-clock/static/css/main.a4adac04.chunk.css"
+  },
+  {
+    "revision": "b6ad02cb9f6912b0c34a",
+    "url": "/react-digital-clock/static/js/2.c2e440f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-digital-clock/static/js/2.c2e440f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfbcabd31b36edb32776",
+    "url": "/react-digital-clock/static/js/main.20c8f351.chunk.js"
+  },
+  {
+    "revision": "5f4780e820b7f6ed5ddb",
+    "url": "/react-digital-clock/static/js/runtime-main.a864dd33.js"
+  }
+]);
